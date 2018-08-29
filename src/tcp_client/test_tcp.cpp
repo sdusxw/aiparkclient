1b4c028bca7f10b906ecc811437300cc1bdf7ca5
sdusxw <sxw@sdboon.com>
2018-08-29 11:37:19 +0800

    client 10

--- src/tcp_client/test_tcp.cpp
+++ src/tcp_client/test_tcp.cpp
@@ -1,10 +1,5 @@
-#include <scy/application.h>
-#include <scy/net/sslsocket.h>
-#include <scy/net/tcpsocket.h>
-
-
-using std::endl;
-using namespace scy;
+#include "common/net_tcp.h"
+#include <iostream>
 
 
 const uint16_t TcpPort = 7666;
@@ -16,14 +11,16 @@ using namespace std;
 int main(int argc, char** argv)
 {
     
-    net::TCPSocket tcp;
+
     //net::Address server("117.50.44.92", 7666);
-    
-    tcp.connect("117.50.44.92", 7666);
-    tcp.setNoDelay(true);
+    NetTcpClient tcp;
+    tcp.connect_server("117.50.44.92", 7666);
+
     std::string msg = "{\"cmd\":\"init_parkid\",\"park_id\":\"0531100015\"}";
-    ssize_t n = tcp.send(msg.c_str(), msg.length());
+    std::string recv_msg;
+    ssize_t n = tcp.send_data(msg, recv_msg);
     cout << "sent:\t" << n << endl;
     cout << msg << endl;
+    cout << recv_msg << endl;
     return 0;
 }
