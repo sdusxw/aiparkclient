4d5ee8ac07e4bc25802efbd6d6e046e6376b32bf
sdusxw <sxw@sdboon.com>
2018-08-29 11:17:06 +0800

    client 9

--- src/tcp_client/test_tcp.cpp
+++ src/tcp_client/test_tcp.cpp
@@ -17,13 +17,13 @@ int main(int argc, char** argv)
 {
     
     net::TCPSocket tcp;
-    net::Address server("117.50.44.92", 7666);
+    //net::Address server("117.50.44.92", 7666);
     
-    tcp.connect(server);
-    
+    tcp.connect("117.50.44.92", 7666);
+    tcp.setNoDelay(true);
     std::string msg = "{\"cmd\":\"init_parkid\",\"park_id\":\"0531100015\"}";
-    ssize_t n = tcp.send(msg.c_str(), msg.length(), server);
+    ssize_t n = tcp.send(msg.c_str(), msg.length());
     cout << "sent:\t" << n << endl;
-    
+    cout << msg << endl;
     return 0;
 }
