e08d91a6fbc592bba0595f3b73ef63a7fd98019f
sdusxw <sxw@sdboon.com>
2018-09-01 11:37:15 +0800

    test client 2

--- src/tcp_client/test_tcp.cpp
+++ src/tcp_client/test_tcp.cpp
@@ -9,7 +9,7 @@ NetTcpClient tcp;
 
 void *heartbeat(void* args)
 {
-    std::string msg = "{\"cmd\":\"heartbeat\",\"park_id\":\"0531100015\"}";
+    std::string msg = "{\"cmd\":\"heartbeat\",\"park_id\":\"0531888888\"}";
     while(true)
     {
         sleep(10);
@@ -27,7 +27,7 @@ int main(int argc, char** argv)
     //net::Address server("117.50.44.92", 7666);
     tcp.connect_server("117.50.44.92", 7666);
 
-    std::string msg = "{\"cmd\":\"init_parkid\",\"park_id\":\"0531100015\"}";
+    std::string msg = "{\"cmd\":\"init_parkid\",\"park_id\":\"0531888888\"}";
     std::string recv_msg;
     ssize_t n = tcp.send_only(msg);
     cout << "sent:\t" << n << endl;
